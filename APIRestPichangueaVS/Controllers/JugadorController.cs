5251f6a5fcaa35ca7f48f772da1907d8178f69d2
PanchoLove <fmunoz94@gmail.com>
2017-10-18 19:36:34 -0300

    Controlador jugador totalmente creado y comentado

--- APIRestPichangueaVS/Controllers/JugadorController.cs
+++ APIRestPichangueaVS/Controllers/JugadorController.cs
@@ -10,48 +10,170 @@ namespace APIRestPichangueaVS.Controllers
 {
     public class JugadorController : ApiController
     {
-        public IEnumerable<Jugador> Get()
+        //Funcion que retorna la lista de jugadores
+        public HttpResponseMessage Get()
         {
-            using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
+            try
             {
-                return entities.Jugador.ToList();
+                //Se obtienen los modelos de la BD
+                using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
+                {
+                    //Se crea una lista con todos los jugadores
+                    var entity = entities.Jugador.ToList();
+                    if (entity != null)
+                    {
+                        //Se retorna el estado OK y la lista de jugadores
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                    else
+                    {
+                        //Se retorna el estado NotFound y un string que indica el error
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No existen jugadores");
+                    }
+                }
+
             }
+            catch (Exception ex)
+            {
+                //En caso de existir otro error, se envia estado de error y un mensaje
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
         }
 
+        //Funcion que retorna un jugador
         public HttpResponseMessage Get(int id)
         {
-            using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
+            try
             {
-                var entity = entities.Jugador.FirstOrDefault(e => e.idJugador == id);
-                if(entity != null)
+                //Se obtienen los modelos de la BD
+                using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    //Se crea una variable con el jugador correspondiente a la ID
+                    var entity = entities.Jugador.FirstOrDefault(e => e.idJugador == id);
+                    if (entity != null)
+                    {
+                        //Se retorna el estado OK y el jugador
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                    else
+                    {
+                        //Se retorna el estado NotFound y un string que indica el error
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Jugador con ID: " + id.ToString() + " no existe");
+                    }
                 }
-                else
-                {
-                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Jugador con ID: " + id.ToString() + " no existe");
-                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de existir otro error, se envia estado de error y un mensaje
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
 
+        //Funcion que agrega un jugador
         public HttpResponseMessage Post([FromBody]Jugador jugador)
         {
             try
             {
+                //Se obtienen los modelos de la BD
                 using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
                 {
+                    //Se agrega el jugador a las entidades
                     entities.Jugador.Add(jugador);
                     entities.SaveChanges();
+                    //Se crea un un mensaje con el codigo Created y con el jugador ingresado
                     var message = Request.CreateResponse(HttpStatusCode.Created, jugador);
+                    //Se concatena la ID al jugador del mensaje
                     message.Headers.Location = new Uri(Request.RequestUri + jugador.idJugador.ToString());
                     return message;
                 }
             }
             catch(Exception ex)
             {
+                //En caso de existir otro error, se envia estado de error y un mensaje
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
 
+        }
+
+        //Funcion que modifica un jugador
+        public HttpResponseMessage Put(int id, [FromBody]Jugador jugador)
+        {
+            try
+            {
+                //Se obtienen los modelos de la BD
+                using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
+                {
+                    //Se crea una variable con el jugador correspondiente a la ID
+                    var entity = entities.Jugador.FirstOrDefault(e => e.idJugador == id);
+                    if (entity == null)
+                    {
+                        //Se retorna el estado NotFound y un string que indica el error
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Jugador con ID: " + id.ToString() + " no existe, no es posible actualizar");
+                         
+                    }
+                    else
+                    {
+                        //Se modifican los campos del jugador
+                        entity.jugApodo = jugador.jugApodo;
+                        entity.jugCelular = jugador.jugCelular;
+                        entity.jugCreacion = jugador.jugCreacion;
+                        entity.jugEmail = jugador.jugEmail;
+                        entity.jugFono = jugador.jugFono;
+                        entity.jugFoto = jugador.jugFoto;
+                        entity.jugMaterno = jugador.jugMaterno;
+                        entity.jugNombre = jugador.jugNombre;
+                        entity.jugPassword = jugador.jugPassword;
+                        entity.jugPaterno = jugador.jugPaterno;
+                        entity.jugRut = jugador.jugRut;
+                        entity.jugRutDv = jugador.jugRutDv;
+                        entity.jugUsername = jugador.jugUsername;
+
+                        //Se guardan los cambios
+                        entities.SaveChanges();
+                        //Se retorna el estado OK y el jugador
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de existir otro error, se envia estado de error y un mensaje
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        //Funcion que elimina un jugador
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                //Se obtienen los modelos de la BD
+                using (PichangueaUsachEntities entities = new PichangueaUsachEntities())
+                {
+                    //Se crea una variable con el jugador correspondiente a la ID
+                    var entity = entities.Jugador.FirstOrDefault(e => e.idJugador == id);
+                    if (entity == null)
+                    {
+                        //Se retorna el estado NotFound y un string que indica el error
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Jugador con ID: " + id.ToString() + " no existe, no es posible actualizar");
+
+                    }
+                    else
+                    {
+                        //Se elimina de la BD el jugador
+                        entities.Jugador.Remove(entity);
+                        entities.SaveChanges();
+                        //Se retorna el estaado OK
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de existir otro error, se envia estado de error y un mensaje
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
         }
     }
 }
